019a21ff971d25edeb47674001057d9246356ff9
JProd <rodriguezjoshuapaul@gmail.com>
2017-11-27 01:16:32 -0700

    Edit commenting and docstring

--- rowreducedEchelon.py
+++ rowreducedEchelon.py
@@ -17,17 +17,18 @@ matrix2 = np.array([[2., 5., 3., 2., 41.],
                     [7., 3., 1., 5., 52.]])
 
 def rref(arrayMatrix):
-    """ Reduces the first row to a [1., x1, x2, ... , xn] form """
+    """ Modifys the given matrix to row reduced echelon form """
+    # Reduces the first row to a [1., x1, x2, ... , xn] form
     arrayMatrix[0] = np.divide(arrayMatrix[0], arrayMatrix[0,0])
-    """ If it is not the last row, preform other row operations  """
+    # If it is not the last row, preform other row operations
     if (len(arrayMatrix) > 1):
-        """ Reduces the other rows to a [0., x1, ... , xn] form """
+        # Reduces the other rows to a [0., x1, ... , xn] form
         arrayMatrix[1:] = np.array([arrayMatrix[row] - 
             (arrayMatrix[row,0] * arrayMatrix[0]) 
             for row in range(1, len(arrayMatrix))])
-        """ Recurses with a bottom right sub-matrix"""
+        # Recurses with a bottom right sub-matrix
         rref(arrayMatrix[1:,1:])
-        """ Reduces the first row to a [1., 0., 0., ... , xn] form """
+        # Reduces the first row to a [1., 0., 0., ... , xn] form 
         arrayMatrix[0] = np.array([arrayMatrix[0] - 
             sum([arrayMatrix[0,i] * arrayMatrix[i]
             for i in range(1, len(arrayMatrix))])])
